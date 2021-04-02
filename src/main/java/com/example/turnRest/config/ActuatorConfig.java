b75c05360637c4f6000693a65547bee62d5bfaa2
Yan Ng <Yan Ng@LAPTOP-3HPMGJGB>
2021-04-03 01:01:20 +0800

    Added actuator config to make httptrace work

---
+++ src/main/java/com/example/turnRest/config/ActuatorConfig.java
@@ -0,0 +1,16 @@
+package com.example.turnRest.config;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfig {
+
+  @Bean
+  public HttpTraceRepository httpTraceRepository() {
+      return new InMemoryHttpTraceRepository();
+  }
+  
+}
